020db06741bdeff60017fb332dedc87a7b543932
Sergio Torrado <sergio.itp16@gmail.com>
2021-05-24 13:46:34 -0500

    Added solution for add two very large numbers codeo's problems

---
+++ codeo/0x32/README.md
@@ -0,0 +1,3 @@
+# Codeo: 0x32 - Sumar dos números muy grandes
+
+https://codeo.app/problemas/0x32-sumar-dos-numeros-muy-grandes
---
+++ codeo/0x32/main.py
@@ -0,0 +1,22 @@
+x = [int(i) for i in input()]
+y = [int(i) for i in input()]
+
+if len(x) < len(y):
+    x, y = y, x
+
+x.reverse()
+
+k = 0
+for i in range(0, len(x)):
+    if y:
+        k += y.pop()
+    v = x[i] + k
+    x[i] = str(v % 10)
+    k = v // 10
+    
+x.reverse()
+
+if k > 0:
+    x.insert(0, "1")
+
+print("".join(x))

6c6b1cb261b97ab30f99c50777e878c49ca3603d
Sergio Torrado <sergio.itp16@gmail.com>
2021-06-12 19:31:05 -0500

    Added solutions to codeforces problems

--- codeforces/problemset/1474B/main.cpp
+++ codeforces/problemset/1474B/main.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 #include <cmath>
 
 using namespace std;
+
+bool isPrime(long x) {
+    if (x < 0) return isPrime(-x);
+    if (x < 5 || x % 2 == 0 || x % 3 == 0) return x == 2 || x == 3;
+    long max = sqrt(x) + 2;
+    long i = 5;
+    while (i < max) {
+        if (x % i == 0 || x % (i + 2) == 0) return false;
+        i += 6;
+    }
+    return true;
+}
 
 int main(int argc, char const *argv[])
 {
     int t;
     cin >> t;
-    long double n, k;
-    for(int i=0; i<t; i++) {
-        cin >> n >> k;
-        k = ceil(n / k) * k;
-        cout << int(ceil(k / n)) << endl;
+    for (int i = 0; i < t; i++) {
+        long d;
+        cin >> d;
+        long a = d + 1;
+        while(!isPrime(a)) a++;
+        long b = a + d;
+        while(!isPrime(b)) b++;
+        cout << (a * b) << endl;
     }
-    return 0;
 }

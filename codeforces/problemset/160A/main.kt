b449f6b12cf51c050a0595bf839eea5848355978
Sergio Torrado <sergio.itp16@gmail.com>
2021-06-20 15:32:23 -0500

    Added solutions to codeforces problems

--- codeforces/problemset/160A/main.kt
+++ codeforces/problemset/160A/main.kt
@@ -8,5 +8,16 @@ private fun readLongs() = readStrings().map { it.toLong() } // list of longs
 private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
 
 fun main() {
-    println("Hello world");
+    val n = readInt()
+    val a = readInts().sorted()
+    var sum = a.sum()
+    var mid = (sum / 2).toInt()
+    if (sum % 2 == 0) mid--
+    var coins = 0
+    for (i in n - 1 downTo 0) {
+        if (sum <= mid) break
+        coins++
+        sum -= a[i]
+    }
+    println(coins)
 }
---
+++ codeforces/problemset/486A/README.md
@@ -0,0 +1,17 @@
+# A. Calculating Function
+
+> implementation, math
+
+> https://codeforces.com/problemset/problem/486/A
+
+For a positive integer n let's define a function f:
+
+f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+Your task is to calculate f(n) for a given integer n.
+
+Input
+The single line contains the positive integer n (1 ≤ n ≤ 1015).
+
+Output
+Print f(n) in a single line.

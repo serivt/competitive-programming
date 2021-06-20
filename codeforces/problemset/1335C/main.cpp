e1bf4f5cd1bb471f1de2d128f1b9b376ccf246a7
Sergio Torrado <sergio.itp16@gmail.com>
2021-06-19 20:01:53 -0500

    Added solutions to codeforces problems

---
+++ codeforces/problemset/1335C/main.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/* Utilities */
+#define sfor(i, n) for (int i = 0; i < n; i++)
+#define forin(i, j, k, in) for (int i = j; i < k; i += in)
+#define foreach(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define pa(i, arr, n) \
+    sfor(i, n) { cout << arr[i] << " "; }
+#define pael(i, arr, n) \
+    sfor(i, n) { cout << arr[i] << endln; }
+#define in(a, b, c) b <= a &&a <= c
+#define clr(x) memset(x, 0, sizeof(x))
+#define endln "\n"
+#define pb push_back
+#define ll long long
+#define ul unsigned long
+
+/* Constants */
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        ul n;
+        cin >> n;
+        ul a[n];
+        unordered_map<int, size_t> s;
+        sfor(i, n) {
+            cin >> a[i];
+            s[a[i]]++;
+        }
+        ul m = 0;
+        for (auto &i:s) {
+            m = max((ul)i.second, m);
+        }
+        ul ss = s.size();
+        cout << (min(ss, m) - (ss == m))<< endln;
+    }
+}

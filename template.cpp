6fe45230e56b66850ac9222f0c50cbe105324d24
Sergio Torrado <sergio.itp16@gmail.com>
2021-07-19 17:27:28 -0500

    Updated cpp template

--- template.cpp
+++ template.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 /* Utilities */
 #define fr(i, n) for (int i = 0; i < n; i++)
-#define forin(i, j, k, in) for (int i = j; i < k; i += in)
+#define forin(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
 #define foreach(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define in(a, b, c) b <= a &&a <= c
+#define in(a, b, c) a <= b && b <= c
 #define clr(x) memset(x, 0, sizeof(x))
 #define endln "\n"
+#define println() cout << "\n"
 #define pb push_back
 #define ll long long
 #define ul unsigned long
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define readArray(v) fr(i, sizeof(v)) cin >> v[i]
 
 /* Constants */
 #define INF (int)1e9
